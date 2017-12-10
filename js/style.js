6f19017477ed7579c596bbfb87985985c3a7801b
Elyssa Albert <albert.elyssa@gmail.com>
2017-12-10 18:29:48 -0500

    Getting Ready

--- js/style.js
+++ js/style.js
@@ -3,18 +3,18 @@ $(document).ready(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 1) {
 			
-			$(".siteheader.stickyheader").css("background", "#F4EEEB");
+			$(".siteheader.stickyheader").css("background", "#F2EEEB");
 			$('a.heading-link-center').fadeOut(100);
 			$('a.heading-link-left').fadeOut(100);
 			$(".wellthyarticleheader").css("color", "rgba(0,0,0,0)");
-			$("p.articletitleappear").css("margin-top", "0.25rem");
+			$("p.articletitleappear").css("margin-top", "0rem");
 			
 	  } else {
 			
 			$(".siteheader.stickyheader").css("background", "transparent");
 			$('a.heading-link-center').fadeIn(100);
 			$('a.heading-link-left').fadeIn(100);
-			$(".wellthyarticleheader").css("color", "#645D59");
+			$(".wellthyarticleheader").css("color", "#525151");
 			$("p.articletitleappear").css("margin-top", "3rem");
 		}
   })
