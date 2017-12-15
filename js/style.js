d532bfde0952d14f5543161bd08766cd411d6552
Elyssa Albert <albert.elyssa@gmail.com>
2017-12-14 22:24:37 -0500

    FINALLY THE NAV IS FIXED SORT-OF

--- js/style.js
+++ js/style.js
@@ -1,21 +1,21 @@
-$(document).ready(function(){
-  $(window).scroll(function(){
+$(document).ready(function () {
+  $(window).scroll(function () {
   	var scroll = $(window).scrollTop();
 	  if (scroll > 1) {
 			
 			$(".siteheader.stickyheader").css("background", "#F2EEEB");
-			$('a.heading-link-center').fadeOut(100);
-			$('a.heading-link-left').fadeOut(100);
+			$('ul.nav-center').fadeOut(250);
+			$('li.instagram').fadeOut(250);
 			$(".wellthyarticleheader").css("color", "rgba(0,0,0,0)");
 			$("p.articletitleappear").css("margin-top", "0rem");
 			
 	  } else {
 			
 			$(".siteheader.stickyheader").css("background", "transparent");
-			$('a.heading-link-center').fadeIn(100);
-			$('a.heading-link-left').fadeIn(100);
+			$('ul.nav-center').fadeIn(250);
+			$('li.instagram').fadeIn(250);
 			$(".wellthyarticleheader").css("color", "#525151");
-			$("p.articletitleappear").css("margin-top", "3rem");
+			$("p.articletitleappear").css("margin-top", "1rem");
 		}
   })
 })
